1226e1ffdc5977132687c4853e9394a96251e2f5
jin <apple380g@gmail.com>
2022-08-26 14:08:04 +0900

    1st commit

---
+++ Java3rdPrj/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+public static void main (String[] args){
+int kor1, kor2, kor3;
+int total;
+float avg;
+
+kor1 = 90;
+kor2 = 60;
+kor3 = 80;
+
+total = kor1 + kor2 + kor3;
+avg = total / 3;
+System.out.printf("totl is %d\n", total);
+System.out.printf("avg is %f\n", avg);
+}
+}
