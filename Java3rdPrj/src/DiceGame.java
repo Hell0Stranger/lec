1026afad7c3533b69f7ad28595279596640e4bc2
jin <apple380g@gmail.com>
2022-08-30 16:34:41 +0900

    2022-08-30 난수생성을 이용한 주사위게임 만들어보기

---
+++ Java3rdPrj/src/DiceGame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+class Dice {
+	int rollValue;
+
+	int rollTheDice() {
+		Random rand = new Random();
+		int rollValue = rand.nextInt(6) + 1;
+		return rollValue;
+	}
+}
+
+class DiceGame {
+	public static void main(String[] args) {
+		Dice dice = new Dice();
+		int rolled1 = dice.rollTheDice();
+		int rolled2 = dice.rollTheDice();
+		int sum = rolled1 + rolled2;
+		if (rolled1 == rolled2) {
+			sum = sum * 2;
+			System.out.println("Double Score '"+rolled1+"' 당첨★ " + sum);
+
+		} else {
+			System.out.println("첫번째로 던진 주사위는" + rolled1);
+			System.out.println("두번째로 던진 주사위는" + rolled2);
+
+			System.out.println("Score" + sum);
+
+		}
+	}
+
+}
